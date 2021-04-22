ede122a948a3ef5918bd79e8e4570a55d375bdad
lebron.a <lebron.a@infox.de>
2021-04-22 18:12:36 -0400

    Added goals input and output

--- App.js
+++ App.js
@@ -1,21 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, TextInput, Button, Text } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  // function goalInputHandler(enteredText) {
+  //   setEnteredGoal(enteredText);
+  // }
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  }
+
+  const addGoalHandler = () => {
+    // this way, it guarantees the latest list of goals
+    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={ styles.screen }>
+      {/* input area  */}
+      <View style={ styles.inputContainer }>
+        <TextInput 
+          placeholder="Course Goal" 
+          style={ styles.input } 
+          // not adding a parenthesis to the function call tells react native to run it only on every key stroke
+          onChangeText={ goalInputHandler }
+          value={ enteredGoal }/>
+        <Button title="ADD" onPress={ addGoalHandler }/>
+      </View>
+      {/* lists of goals */}
+      <View>
+        {courseGoals.map((goal) => (
+          <View key={goal} style={styles.listItem}>
+            <Text>{goal}</Text>
+          </View>
+        ))}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  // name of the javascript Object, it can be any name
+  screen: {
+    padding: 50
   },
+  inputContainer: {
+    flexDirection:'row', 
+    justifyContent:'space-between', 
+    alignItems:'center'
+  },
+  input: {
+    width:'80%', 
+    borderBottomColor:'black', 
+    borderBottomWidth:1, 
+    padding:10
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1
+  }
 });
